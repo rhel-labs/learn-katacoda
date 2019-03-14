6e88f2c9d83139b3d000be4c6b3ebd051bcbe2c1
Scott McBrien <smcbrien@smcbrien.smcbrien.csb>
2019-03-14 14:30:16 -0400

    further removing spinner-related code

--- webconsole-software/background.sh
+++ webconsole-software/background.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "done" >> /root/katacoda-finished
-
 yum -y install cockpit cockpit-packagekit
 
 systemctl start cockpit
@@ -9,5 +7,3 @@ systemctl start cockpit
 usermod -aG wheel rhel
 
 echo redhat | passwd --stdin rhel
-
-echo "done" >> /root/katacoda-background-finished
