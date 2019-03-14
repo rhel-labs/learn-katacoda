2460dd03458cc6f2cbddf6ca439acd359165c2d5
Scott McBrien <smcbrien@smcbrien.smcbrien.csb>
2019-03-14 14:31:30 -0400

    re-ordering

--- webconsole-software/background.sh
+++ webconsole-software/background.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-yum -y install cockpit cockpit-packagekit
-
-systemctl start cockpit
-
 usermod -aG wheel rhel
 
 echo redhat | passwd --stdin rhel
+
+yum -y install cockpit cockpit-packagekit
+
+systemctl start cockpit
