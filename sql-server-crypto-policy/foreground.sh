fbc3b655055842bf90abb58393b62189f8a98a30
Don Pinto <dopinto@dopinto.remote.csb>
2019-08-01 14:56:01 -0400

    Making changes to add raw file path

--- sql-server-crypto-policy/foreground.sh
+++ sql-server-crypto-policy/foreground.sh
@@ -1,4 +1,4 @@
-curl -o /root/install_sql.sh https://github.com/rhel-labs/learn-katacoda/blob/master/sql-server-crypto-policy/install_sql.sh
+curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/install_sql.sh 
 sudo chmod +x ./install_sql.sh
 sudo ./install_sql.sh
 
