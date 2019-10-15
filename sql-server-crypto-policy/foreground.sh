da69f1f5d012a22052094b32cea42fb924f6cc02
dpredhat <51135890+dpredhat@users.noreply.github.com>
2019-10-15 15:18:02 -0400

    Update foreground.sh

--- sql-server-crypto-policy/foreground.sh
+++ sql-server-crypto-policy/foreground.sh
@@ -1,8 +1,8 @@
 curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/install_sql.sh 
 sudo chmod +x ./install_sql.sh
-sudo ./install_sql.sh
-export HOSTNAME=`hostname --all-fqdns`
 
-echo "Getting insights breakscript for insights demo"
 curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/insights_breakscript.sh 
 sudo chmod +x ./insights_breakscript.sh
+
+sudo ./install_sql.sh
+export HOSTNAME=`hostname --all-fqdns`
