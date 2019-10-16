7d3de0436a82b898761c1abcce696325ea20289f
dpredhat <51135890+dpredhat@users.noreply.github.com>
2019-10-16 10:48:49 -0400

    Update foreground.sh

--- sql-server-crypto-policy/foreground.sh
+++ sql-server-crypto-policy/foreground.sh
@@ -4,5 +4,5 @@ sudo chmod +x ./install_sql.sh
 curl -o /root/insights_breakscript.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/insights_breakscript.sh 
 sudo chmod +x ./insights_breakscript.sh
 
-#sudo ./install_sql.sh
-#export HOSTNAME=`hostname --all-fqdns`
+sudo ./install_sql.sh
+export HOSTNAME=`hostname --all-fqdns`
