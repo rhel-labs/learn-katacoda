293ae9083da100dbab5ac8904999778ce82fe4fc
dpredhat <51135890+dpredhat@users.noreply.github.com>
2019-10-16 10:20:40 -0400

    Update insights_breakscript.sh

--- sql-server-crypto-policy/insights_breakscript.sh
+++ sql-server-crypto-policy/insights_breakscript.sh
@@ -12,22 +12,23 @@ MSSQL_PID='evaluation'
 
 if [ -z $MSSQL_SA_PASSWORD ]
 then
-  echo "Environment variable MSSQL_SA_PASSWORD must be set for unattended install"
+  echo Environment variable MSSQL_SA_PASSWORD must be set for unattended install
   exit 1
 fi
 
 echo Reading command line parameter to break setup and trigger insights
 if [ "$1" == "1" ] 
 then
-    echo "Positional parameter 1 contains 1"
+    echo Positional parameter 1 contains 1
 
 elif [ "$1" == "2" ]
 then
-    echo "Positional parameter 1 contains 2"
+    echo Positional parameter 1 contains 2
 
 elif [ "$1" == "3" ]
 then
-    echo "Positional parameter 1 contains 3"
+    echo Positional parameter 1 contains 3
+
 fi
 
 # Restart SQL Server after installing:
