de74d8819a5d2500259059588009164caa21aa5b
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-02-05 17:27:57 -0500

    Update foreground.sh

--- sql-server-crypto-policy/foreground.sh
+++ sql-server-crypto-policy/foreground.sh
@@ -1,4 +1,4 @@
-curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/install_sql.sh 
+curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/install_sql2019.sh 
 sudo chmod +x ./install_sql.sh
 
 curl -o /root/insights_breakscript.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/insights_breakscript.sh 
