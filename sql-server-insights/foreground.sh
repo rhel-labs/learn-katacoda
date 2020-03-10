514bc48274f18c1640e678c98ff207eb7fe57bef
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-03-09 23:54:26 -0400

    Update foreground.sh

--- sql-server-insights/foreground.sh
+++ sql-server-insights/foreground.sh
@@ -1,4 +1,4 @@
-curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/install_sql2019.sh 
+curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/install_sql.sh 
 sudo chmod +x ./install_sql.sh
 
 sudo ./install_sql.sh
