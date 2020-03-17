df85da91272d993e8772f9d4059a368bca7ec575
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-03-17 18:04:09 -0400

    Update background.sh

--- sql-server-ansible/background.sh
+++ sql-server-ansible/background.sh
@@ -1,9 +1,11 @@
 !#/bin/bash
 
-wget https://github.com/rhel-labs/learn-katacoda/blob/master/sql-server-ansible/site.yml
-wget https://github.com/rhel-labs/learn-katacoda/blob/master/sql-server-ansible/ansible.cfg
+touch /root/post-run.log
 
-echo "adding ansible repo" > /root/post-run.log
+wget https://github.com/rhel-labs/learn-katacoda/blob/master/sql-server-ansible/site.yml &>> /root/post-run.log
+wget https://github.com/rhel-labs/learn-katacoda/blob/master/sql-server-ansible/ansible.cfg &>> /root/post-run.log
+
+echo "adding ansible repo" >> /root/post-run.log
 subscription-manager repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms &>> /root/post-run.log
 
 echo "installing ansible" >> /root/post-run.log
