cd9922742ea57edc8db60bc7303ae7468f2c16e3
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-03-17 16:03:30 -0400

    Update background.sh

--- sql-server-ansible/background.sh
+++ sql-server-ansible/background.sh
@@ -6,4 +6,5 @@ subscription-manager repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms &>> /root
 echo "installing ansible" >> /root/post-run.log
 yum -y install ansible &>> /root/post-run.log
 
-export HOST=`hostname`
+echo "Settting hostname env" >> /root/post-run.log
+export HOST=`hostname` &>> /root/post-run.log
