788d95e2092bdcdb45de2c25efbee00023139216
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-03-18 08:41:19 -0400

    Update background.sh

--- sql-server-ansible/background.sh
+++ sql-server-ansible/background.sh
@@ -2,8 +2,8 @@
 
 touch /root/post-run.log
 
-wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-ansible/site.yml &>> /root/post-run.log
-wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-ansible/ansible.cfg &>> /root/post-run.log
+#wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-ansible/site.yml &>> /root/post-run.log
+#wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-ansible/ansible.cfg &>> /root/post-run.log
 
 echo "adding ansible repo" >> /root/post-run.log
 subscription-manager repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms &>> /root/post-run.log
@@ -14,4 +14,4 @@ yum -y install ansible &>> /root/post-run.log
 echo "Setting hostname env" >> /root/post-run.log
 export HOSTNAME=`hostname --all-fqdns` &>> /root/post-run.log
 
-sed -i -e "s/node/$HOSTNAME/g" site.yml &>> /root/post-run.log
+#sed -i -e "s/node/$HOSTNAME/g" site.yml &>> /root/post-run.log
