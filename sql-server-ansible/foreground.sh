d83699d5e166322a8de6ecdd1faff10e23a50f9c
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-03-19 09:30:57 -0400

    Update foreground.sh

--- sql-server-ansible/foreground.sh
+++ sql-server-ansible/foreground.sh
@@ -1,8 +1,16 @@
 !#/bin/bash
+
+n=1
 while [ ! -f /root/post-run.log.done ] ;
 do
+      if test "$n" = "1"
+      then
+	      clear
+            n=$(( n+1 ))	 # increments $n
+      else
+	      printf "."
+      fi
       sleep 2
-      printf "."
 done
 clear
-echo "Ready to start your scenario"
+printf "Ready to start your scenario\n"
