ef7e02a3514702e1a2d765d6895c919fa5cc9446
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-03-23 17:56:58 -0400

    Update background.sh

--- sql-server-cstore/background.sh
+++ sql-server-cstore/background.sh
@@ -7,9 +7,13 @@ touch /root/post-run.log
 wget https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak
 mv WideWorldImporters-Full.bak WW.bak
 
+#Download the SQL Query script
+wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-cstore/CS.sql
+
 #Download the install script and run it
 curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/install_sql2019.sh
 sudo chmod +x ./install_sql.sh
+
 
 sudo ./install_sql.sh &>> /root/post-run.log
 export HOSTNAME=`hostname --all-fqdns`
