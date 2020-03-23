d34c7ac0307949aba6c1050e865dc5039d2dda1f
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-03-23 17:38:48 -0400

    Update background.sh

--- sql-server-cstore/background.sh
+++ sql-server-cstore/background.sh
@@ -6,8 +6,6 @@ touch /root/post-run.log
 #Download the WWImporters database
 wget https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak
 mv WideWorldImporters-Full.bak WW.bak
-mkdir /var/opt/mssql/backup/
-mv /root/WW.bak /var/opt/mssql/backup/
 
 #Download the install script and run it
 curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/install_sql2019.sh
@@ -20,6 +18,10 @@ firewall-cmd --zone=public --add-service=https --permanent
 firewall-cmd --reload
 
 systemctl --now enable httpd
+
+#Move files to backup folder under /var/opt/mssql
+mkdir /var/opt/mssql/backup/
+mv /root/WW.bak /var/opt/mssql/backup/
 
 #Create a done file to signal we have finished
 touch /root/post-run.log.done
