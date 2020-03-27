411b379572cb45aa161d6e933adcca8e1c072409
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-03-27 12:21:45 -0400

    Update CS2.sql

--- sql-server-cstore/CS2.sql
+++ sql-server-cstore/CS2.sql
@@ -17,24 +17,25 @@ a.a * 10 AS Price,
 CONCAT(a.a, N' ', b.a, N' ', c.a, N' ', d.a, N' ', e.a, N' ', f.a, N' ', g.a, N' ', h.a) AS ProductName
 INTO Orders 
 FROM a, a AS b, a AS c, a AS d, a AS e, a AS f, a AS g, a AS h; 
+                                                                               
+CREATE NONCLUSTERED COLUMNSTORE INDEX [columnstoreindex] ON Orders
+(
+  [Price]
+) WITH (DROP_EXISTING = ON, COMPRESSION_DELAY = 0);
+GO
 
-CREATE CLUSTERED COLUMNSTORE INDEX columnstoreindex ON Orders;
-
-DBCC FREEPROCCACHE;
+DBCC DROPCLEANBUFFERS;
 GO
                                                                                
 DECLARE @StartingTime datetime2(7) = SYSDATETIME();
 
-SELECT SUM(Price) FROM Orders;
+SELECT SUM(Price), AVG(Price) FROM Orders;
 
 PRINT 'Using nonclustered columnstore index: ' + CAST(DATEDIFF(millisecond, @StartingTime, SYSDATETIME()) AS varchar(20)) + ' ms';
-                                                                                                                    
-DBCC FREEPROCCACHE;
-GO
 
 SET @StartingTime = SYSDATETIME();
 
-SELECT SUM(Price) FROM Orders 
+SELECT SUM(Price), AVG(Price) FROM Orders 
 OPTION (IGNORE_NONCLUSTERED_COLUMNSTORE_INDEX);
 
 PRINT 'Without nonclustered columnstore index: ' + CAST(DATEDIFF(millisecond, @StartingTime, SYSDATETIME()) AS varchar(20)) + ' ms';
