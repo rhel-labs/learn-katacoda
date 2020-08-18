14cf9575639f91711dca5517a2461a43018e9a14
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-08-18 14:59:23 -0400

    Update background.sh

--- sql-server-ubi/background.sh
+++ sql-server-ubi/background.sh
@@ -2,7 +2,7 @@
 
 touch /root/post-run.log
 
-wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-ubi/InstPubs.sql &>> /root/post-run.log
+wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-ubi/imoltp.sql &>> /root/post-run.log
 
 #Create a done file to signal we have finished
 touch /root/post-run.log.done
