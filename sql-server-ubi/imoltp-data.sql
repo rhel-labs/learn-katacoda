35eb6960c344c3432f2ed4f004a41762cf2e74db
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-08-18 15:08:17 -0400

    Create imoltp-data.sql

---
+++ sql-server-ubi/imoltp-data.sql
@@ -0,0 +1,19 @@
+-- insert data into the tables  
+INSERT dbo.UserSession VALUES (342, SYSDATETIME(), 4);
+INSERT dbo.UserSession VALUES (65, SYSDATETIME(), NULL)   
+INSERT dbo.UserSession VALUES (8798, SYSDATETIME(), 1)   
+INSERT dbo.UserSession VALUES (80, SYSDATETIME(), NULL)   
+INSERT dbo.UserSession VALUES (4321, SYSDATETIME(), NULL)   
+INSERT dbo.UserSession VALUES (8578, SYSDATETIME(), NULL)   
+  
+INSERT dbo.ShoppingCart VALUES (8798, SYSDATETIME(), NULL)   
+INSERT dbo.ShoppingCart VALUES (23, SYSDATETIME(), 45.4)   
+INSERT dbo.ShoppingCart VALUES (80, SYSDATETIME(), NULL)   
+INSERT dbo.ShoppingCart VALUES (342, SYSDATETIME(), 65.4)   
+GO  
+
+-- Verify table contents
+
+SELECT count(*) FROM dbo.UserSession;
+SELECT count(*) FROM dbo.ShoppingCart;
+GO  
