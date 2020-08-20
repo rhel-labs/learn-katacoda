925a67fd3c7c7039be8e54a078dcb40c1e7da338
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-08-20 11:34:56 -0400

    Update background.sh

--- ansible-idm/background.sh
+++ ansible-idm/background.sh
@@ -6,8 +6,5 @@ subscription-manager repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms &>> /root
 echo "installing ansible" >> /root/post-run.log
 yum -y install ansible &>> /root/post-run.log
 
-echo "Yum update all machines using ansible" >> /root/post-run.log
-
-
 #Create a done file to signal we have finished
 touch /root/post-run.log.done
