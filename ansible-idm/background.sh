3a08ac88c49e306e31de63a7b71f67c7938bae9a
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-08-24 12:36:08 -0400

    Update background.sh

--- ansible-idm/background.sh
+++ ansible-idm/background.sh
@@ -6,8 +6,8 @@ subscription-manager repos --enable=ansible-2.8-for-rhel-8-x86_64-rpms &>> /root
 echo "installing ansible" >> /root/post-run.log
 yum -y install ansible &>> /root/post-run.log
 
-#echo "Upgrade machines to latest version of RHEL" >> /root/post-run.log
-#ansible-playbook playbook-upgrade.yml &>> /root/post-run.log
+echo "Upgrade machines to latest version of RHEL" >> /root/post-run.log
+ansible-playbook playbook-upgrade.yml &>> /root/post-run.log
 
 #Create a done file to signal we have finished
 touch /root/post-run.log.done
