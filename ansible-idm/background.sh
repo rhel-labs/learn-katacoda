f7fe6133b7a3a6ae02b0dfffd14c00fe0e5f46c9
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-08-25 15:56:50 -0400

    Update background.sh

--- ansible-idm/background.sh
+++ ansible-idm/background.sh
@@ -9,9 +9,5 @@ yum -y install ansible &>> /root/post-run.log
 echo "Upgrade machines to latest version of RHEL" >> /root/post-run.log
 ansible-playbook playbook-upgrade.yml &>> /root/post-run.log
 
-echo "Copy over the hosts file" >> /root/post-run.log
-scp /etc/hosts root@host02:/etc/hosts
-scp /etc/hosts root@host03:/etc/hosts
-
 #Create a done file to signal we have finished
 touch /root/post-run.log.done
