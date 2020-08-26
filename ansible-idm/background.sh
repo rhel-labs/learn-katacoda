38f1acc7180d72044c94bfc8721860bdcc790c9d
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-08-26 12:30:41 -0400

    Update background.sh

--- ansible-idm/background.sh
+++ ansible-idm/background.sh
@@ -1,6 +1,9 @@
 !#/bin/bash
 
-echo "adding ansible repo" > /root/post-run.log
+echo "Setup activation key" >> /root/post-run.log
+subscription-manager register --org=12451665 --activationkey=idm-lab-temp
+
+echo "adding ansible repo" >> /root/post-run.log
 subscription-manager repos --enable=ansible-2.8-for-rhel-8-x86_64-rpms &>> /root/post-run.log
 
 echo "installing ansible" >> /root/post-run.log
@@ -11,9 +14,9 @@ IP_HOST1=$(hostname -i | awk '{print $NF}')
 IP_HOST2=$(grep -Ri "host02" /etc/hosts | cut -d" " -f1)
 IP_HOST3=$(grep -Ri "host03" /etc/hosts | cut -d" " -f1)
 
-sed -i 's/%place01%/'"$IP_HOST1"'/' /root/hosts >> /root/post-run.log
-sed -i 's/%place02%/'"$IP_HOST2"'/' /root/hosts >> /root/post-run.log
-sed -i 's/%place03%/'"$IP_HOST3"'/' /root/hosts >> /root/post-run.log
+sed -i 's/%place01%/'"$IP_HOST1"'/' /root/hosts &>> /root/post-run.log
+sed -i 's/%place02%/'"$IP_HOST2"'/' /root/hosts &>> /root/post-run.log
+sed -i 's/%place03%/'"$IP_HOST3"'/' /root/hosts &>> /root/post-run.log
 
 echo "Upgrade machines to latest version of RHEL" >> /root/post-run.log
 ansible-playbook playbook-upgrade.yml &>> /root/post-run.log
