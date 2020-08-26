c10aebd9a338419272dc1df257be02ad14ea928a
dpredhat <51135890+dpredhat@users.noreply.github.com>
2020-08-26 14:07:06 -0400

    Update background.sh

--- ansible-idm/background.sh
+++ ansible-idm/background.sh
@@ -15,6 +15,9 @@ subscription-manager repos --enable=ansible-2.8-for-rhel-8-x86_64-rpms &>> /root
 echo "installing ansible" >> /root/post-run.log
 yum -y install ansible &>> /root/post-run.log
 
+echo "Register hosts02/03" >> /root/post-run.log
+ansible-playbook playbook-register.yml &>> /root/post-run.log
+
 echo "Replace placehold hostnames in ansible config file" >> /root/post-run.log
 IP_HOST1=$(hostname -i | awk '{print $NF}')
 IP_HOST2=$(grep -Ri "host02" /etc/hosts | cut -d" " -f1)
