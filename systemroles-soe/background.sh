bac3795ce4a15e93872ea398017a68646e567949
Scott McBrien <smcbrien@smcbrien.smcbrien.csb>
2020-12-02 06:20:20 -0500

    added ansible repo config to host02 and host03

--- systemroles-soe/background.sh
+++ systemroles-soe/background.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+
+ssh host02 'subscription-manager  repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms'
+ssh host03 'subscription-manager  repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms'
