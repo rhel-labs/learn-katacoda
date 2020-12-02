be77fcd728ee41ea552c0a3ac647546ee78a6b9f
Scott McBrien <smcbrien@smcbrien.smcbrien.csb>
2020-12-02 06:31:16 -0500

    removed repo config, added initial yml content

--- systemroles-soe/background.sh
+++ systemroles-soe/background.sh
@@ -2,5 +2,24 @@
 
 echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 
-ssh host02 'subscription-manager  repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms'
-ssh host03 'subscription-manager  repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms'
+cat << EOF > soe.yml
+---
+- hosts: localhost
+  vars:
+    kernel_settings_sysctl:
+      - name: vm.swappiness
+        value: 20
+      - name: vm.dirty_ratio
+        value: 40
+      - name: vm.dirty_expire_centisecs
+        value: 500
+      - name: vm.dirty_writeback_centisecs
+        value: 100
+      - name: kernel.sem
+        value: "250 32000 100 128"
+    tlog_scope_sssd: all
+  roles:
+    - role: rhel-system-roles.kernel_settings
+    - role: rhel-system-roles.tlog
+EOF
+
