bfb582796839d8cb881d857ba9e481dae2dc258d
dpredhat <51135890+dpredhat@users.noreply.github.com>
2021-03-12 15:02:38 -0500

    Update background.sh

--- pcp-intro/background.sh
+++ pcp-intro/background.sh
@@ -4,17 +4,11 @@
 touch /root/post-run.log
 
 #Download the SQL Query script
-wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-cstore/CSIndex.sql
-wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-cstore/CSNoIndex.sql
-
-mkdir /root/Scripts/
-mv CSIndex.sql /root/Scripts/
-mv CSNoIndex.sql /root/Scripts/
+wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/pcp-intro/createUser.sql
 
 #Download the install script and run it
 curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/install_sql2019.sh
 sudo chmod +x ./install_sql.sh
-
 
 sudo ./install_sql.sh &>> /root/post-run.log
 export HOSTNAME=`hostname --all-fqdns`
