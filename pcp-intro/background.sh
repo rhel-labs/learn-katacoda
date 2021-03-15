dab3d9a3187ad1d5828d3f261496ac0b20a51bdf
dpredhat <51135890+dpredhat@users.noreply.github.com>
2021-03-15 19:36:54 -0400

    Update background.sh

--- pcp-intro/background.sh
+++ pcp-intro/background.sh
@@ -12,7 +12,7 @@ mkdir /var/opt/mssql/backup/
 cp AdventureWorks2019.bak /var/opt/mssql/backup/
 
 #Download the install script and run it
-curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-crypto-policy/install_sql2019.sh
+curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/pcp-intro/install_sql2019.sh
 sudo chmod +x ./install_sql.sh
 
 sudo ./install_sql.sh &>> /root/post-run.log
