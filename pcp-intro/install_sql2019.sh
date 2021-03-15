485dc27584e4a113a9830d01788c3cce91e8f968
dpredhat <51135890+dpredhat@users.noreply.github.com>
2021-03-15 19:56:48 -0400

    Update install_sql2019.sh

--- pcp-intro/install_sql2019.sh
+++ pcp-intro/install_sql2019.sh
@@ -66,6 +66,8 @@ sudo systemctl restart mssql-server
 sudo systemctl status mssql-server --no-pager
 
 #Restore the database
+mkdir /var/opt/mssql/backup/
+cp AdventureWorks2019.bak /var/opt/mssql/backup/
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -Q 'RESTORE DATABASE AdventureWorks2019 FROM DISK = "/var/opt/mssql/backup/AdventureWorks2019.bak" WITH MOVE "AdventureWorks2017" to "/var/opt/mssql/data/AdventureWorks2019.mdf", MOVE "AdventureWorks2017_Log" to "/var/opt/mssql/data/AdventureWorks2019_log.ldf", NOUNLOAD, STATS = 5' 
 
 
