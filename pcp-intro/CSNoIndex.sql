4c1d8e00edac4b199b74b64d16725dd6436e1bbb
dpredhat <51135890+dpredhat@users.noreply.github.com>
2021-03-16 13:25:42 -0400

    Update CSNoIndex.sql

--- pcp-intro/CSNoIndex.sql
+++ pcp-intro/CSNoIndex.sql
@@ -14,7 +14,7 @@ DBCC DROPCLEANBUFFERS;
 GO
 
 WITH a AS (SELECT * FROM (VALUES(1),(2),(3),(4),(5),(6),(7),(8),(9),(10)) AS a(a)) 
-SELECT TOP(5000000) ROW_NUMBER() OVER (ORDER BY a.a) AS OrderItemId, 
+SELECT TOP(500000000) ROW_NUMBER() OVER (ORDER BY a.a) AS OrderItemId, 
 a.a + b.a + c.a + d.a + e.a + f.a + g.a + h.a AS OrderId,
 a.a * 10 AS Price,
 CONCAT(a.a, N' ', b.a, N' ', c.a, N' ', d.a, N' ', e.a, N' ', f.a, N' ', g.a, N' ', h.a) AS ProductName
