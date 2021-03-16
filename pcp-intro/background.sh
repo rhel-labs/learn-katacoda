7a5bccd73ec46916f5e73350773138d6b985ae71
dpredhat <51135890+dpredhat@users.noreply.github.com>
2021-03-15 22:29:12 -0400

    Update background.sh

--- pcp-intro/background.sh
+++ pcp-intro/background.sh
@@ -6,8 +6,9 @@ touch /root/post-run.log
 #Download the SQL Query script
 wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/pcp-intro/createUser.sql
 
-#Download the backup database file
-wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak
+wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-cstore/CSIndex.sql
+mkdir /root/Scripts/
+mv CSIndex.sql /root/Scripts/
 
 #Download the install script and run it
 curl -o /root/install_sql.sh https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/pcp-intro/install_sql2019.sh
@@ -20,9 +21,6 @@ firewall-cmd --zone=public --add-service=https --permanent
 firewall-cmd --reload
 
 systemctl --now enable httpd
-
-#Restore the database
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Redhat1! -Q 'RESTORE DATABASE AdventureWorks2019 FROM DISK = "/var/opt/mssql/backup/AdventureWorks2019.bak" WITH MOVE "AdventureWorks2017" to "/var/opt/mssql/data/AdventureWorks2019.mdf", MOVE "AdventureWorks2017_Log" to "/var/opt/mssql/data/AdventureWorks2019_log.ldf", NOUNLOAD, STATS = 5' 
 
 #Install python and unixODBC dependant packages
 sudo yum install gcc-c++ python3-devel unixODBC-devel -y
