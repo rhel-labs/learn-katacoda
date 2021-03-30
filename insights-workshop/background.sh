08c33b8b3477263756fe89b73f3f60742f9f4b4d
Scott McBrien <smcbrien@smcbrien.smcbrien.csb>
2021-03-30 16:01:44 -0400

    updated background script to remove sshd configuration to create an Advisor recommendation

--- insights-workshop/background.sh
+++ insights-workshop/background.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -ei 's/\# Per.*//g' /etc/ssh/sshd_config
-sed -ei 's/ClientAliveInterval.*900//g' /etc/ssh/sshd_config
+sed -i 's/\# Per.*//g' /etc/ssh/sshd_config
+sed -i 's/ClientAliveInterval.*900//g' /etc/ssh/sshd_config
 
 
