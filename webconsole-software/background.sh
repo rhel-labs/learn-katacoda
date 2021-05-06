4acbde0a71c9abbd2d0a097093c2c6316ec61b03
Scott McBrien <smcbrien@smcbrien.smcbrien.csb>
2021-05-06 11:13:59 -0400

    updated yum.conf to ignore kernel updates

--- webconsole-software/background.sh
+++ webconsole-software/background.sh
@@ -12,4 +12,6 @@ yum -y install cockpit cockpit-packagekit
 echo "starting cockpit" >> /root/post-run.log
 systemctl start cockpit
 
+echo "exclude=kernel*"" >> /etc/yum.conf
+
 echo "DONE" >> /root/post-run.log
