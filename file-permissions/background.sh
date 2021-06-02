7e5e1243b3d8dc53b8fab2db575abd3f2a34ab2e
Jaret Anderson <jaretanderson2@gmail.com>
2021-06-02 14:01:59 -0600

    removed automatic team add so that we can do it in the lab

--- file-permissions/background.sh
+++ file-permissions/background.sh
@@ -3,9 +3,8 @@
 # create guest user account
 useradd guest
 
-# create team group and add guest
+# create team group
 groupadd team
-usermod -a -G team guest
 
 # modify permissions on proprietary dir
 chmod 640 /srv/proprietary
