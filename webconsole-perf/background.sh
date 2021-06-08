6e1c9d1cfb6d03aa060e60c16f25c4c52f163dc8
dpredhat <51135890+dpredhat@users.noreply.github.com>
2021-06-08 13:27:02 -0400

    Update background.sh

--- webconsole-perf/background.sh
+++ webconsole-perf/background.sh
@@ -17,6 +17,13 @@ echo "exclude=kernel*" >> /etc/yum.conf
 echo "upgrading to RHEL 8.4" >> /root/post-run.log
 sudo yum update -y
 
+echo "Install PCP packages" >> /root/post-run.log
+sudo yum install pcp-zeroconf -y
+sudo yum install cockpit-pcp -y
+
+echo "restart cockpit" >> /root/post-run.log
+systemctl restart cockpit
+
 echo "Install Stress-NG" >> /root/post-run.log
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo yum install -y stress-ng
