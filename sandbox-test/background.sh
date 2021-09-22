859a0cd0d26291643c83089d2081dbe9a6e00454
dpredhat <51135890+dpredhat@users.noreply.github.com>
2021-09-22 10:08:25 -0400

    Update background.sh

--- sandbox-test/background.sh
+++ sandbox-test/background.sh
@@ -17,7 +17,7 @@ curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.14.2
 sudo rpm -vi filebeat-7.14.2-x86_64.rpm
 
 echo "configure cred" >> /root/post-run.log
-sed -i 's/#cloud.id:/cloud.id:elastic/g' /etc/filebeat/filebeat.yml
-sed -i 's/#cloud.auth:/cloud.auth:eVFZKQqch39DRL8yUsNHueOQ/g' /etc/filebeat/filebeat.yml
+sed -i 's/#cloud.id:/cloud.id:"Sandbox-Logs:ZWFzdHVzMi5henVyZS5lbGFzdGljLWNsb3VkLmNvbSQxZGUwZjgxMDIyMWM0ZGIwYjVjOWZlZTQ2ZmE4OTQ5YiRhYjViM2FkOTY3NDY0NWM0OTY3OTVjZWNlOWNjM2VmYg=="/g' /etc/filebeat/filebeat.yml
+sed -i 's/#cloud.auth:/cloud.auth: "elastic:eVFZKQqch39DRL8yUsNHueOQ"/g' /etc/filebeat/filebeat.yml
 
 echo "DONE" >> /root/post-run.log
