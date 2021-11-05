44bed88b64468d59170384628adc4a23008cea32
dpredhat <51135890+dpredhat@users.noreply.github.com>
2021-11-05 09:32:32 -0400

    Update background.sh

--- sandbox-test/background.sh
+++ sandbox-test/background.sh
@@ -13,6 +13,9 @@ echo "starting cockpit" >> /root/post-run.log
 systemctl start cockpit
 
 echo "setting up logging" >> /root/post-run.log
+firewall-cmd --permanent --add-port=514/udp
+firewall-cmd --permanent --add-port=514/tcp
+firewall-cmd --reload
 curl -O https://www.loggly.com/install/configure-linux.sh
 sudo bash configure-linux.sh -a rhtestdemo -t 1e39fb71-0ac5-44dd-b559-acb4230a542a -u don
 
