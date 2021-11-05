1aeadc7089b11fa542892805e912ffd0c2cce472
dpredhat <51135890+dpredhat@users.noreply.github.com>
2021-11-05 10:58:13 -0400

    Update background.sh

--- sandbox-test/background.sh
+++ sandbox-test/background.sh
@@ -13,7 +13,7 @@ echo "starting cockpit" >> /root/post-run.log
 systemctl start cockpit
 
 
-echo "setting up logging" >> /root/post-run.log
+echo "getting logging" >> /root/post-run.log
 yum install -y tlog
 sudo rpm --import https://repo.logdna.com/logdna.gpg
 echo "[logdna]
@@ -22,14 +22,14 @@ baseurl=https://repo.logdna.com/el6/
 enabled=1
 gpgcheck=1
 gpgkey=https://repo.logdna.com/logdna.gpg" | sudo tee /etc/yum.repos.d/logdna.repo
+
+echo "setting up agents" >> /root/post-run.log
 sudo yum -y install logdna-agent
-sudo tlog-rec --writer=file --file-path=/var/log/tlog.log
 sudo logdna-agent -k b1f7f85d2b222d70427c214a4faedc67 # this is your unique Ingestion 
-# /var/log is monitored/added by default (recursively), optionally add more dirs with:
-# sudo logdna-agent -d /var/log/slog/tlog.log
-# You can configure the LogDNA Agent to tag your hosts with:
-# sudo logdna-agent -t mytag,myothertag
 sudo chkconfig logdna-agent on
+
+echo "starting up logging" >> /root/post-run.log
+sudo tlog-rec --writer=file --file-path=/var/log/tlog.log
 sudo service logdna-agent start
 
 
