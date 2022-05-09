56e2a5708666a0f62d27237f3f6f18b704b34480
shaneboulden <shane.boulden@gmail.com>
2022-05-10 09:23:53 +1000

    fixed permission for lab user

--- fapolicyd/background.sh
+++ fapolicyd/background.sh
@@ -6,4 +6,7 @@ echo "Setting password" >> /root/post-run.log
 echo redhat | passwd --stdin rhel
 
 echo "Adding cowsay" >> /root/post-run.log
-curl -L https://github.com/Code-Hex/Neo-cowsay/releases/download/v2.0.4/cowsay_2.0.4_Linux_x86_64.tar.gz | tar xvz
+curl -L https://github.com/Code-Hex/Neo-cowsay/releases/download/v2.0.4/cowsay_2.0.4_Linux_x86_64.tar.gz | tar xvz -C /home/rhel
+
+echo "Fixing permissions" >> /root/post-run.log
+chown rhel:rhel /home/rhel/*
