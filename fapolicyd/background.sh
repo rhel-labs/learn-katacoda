6a3125c4cac5e2e76b4786baaf231b51e7653396
shaneboulden <shane.boulden@gmail.com>
2022-05-10 08:43:32 +1000

    added a setup script

---
+++ fapolicyd/background.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Adding wheel" > /root/post-run.log
+usermod -aG wheel rhel
+
+echo "Setting password" >> /root/post-run.log
+echo redhat | passwd --stdin rhel
+
+echo "Adding cowsay" >> /root/post-run.log
+curl -L https://github.com/Code-Hex/Neo-cowsay/releases/download/v2.0.4/cowsay_2.0.4_Linux_x86_64.tar.gz | tar xvz
